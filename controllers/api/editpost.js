635cae082f2c52e2efa40c37e1f99ef28b6567e1
daleyjones <djones_15@hotmail.com>
2023-07-17 20:49:37 +0100

    routes added

--- controllers/api/editpost.js
+++ controllers/api/editpost.js
@@ -1,3 +1,5 @@
+const router = require('express').Router();
+const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
