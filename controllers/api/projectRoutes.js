49fef37de549b3028cd6e5d627af10603277352b
Tiernan <tiernanjamesgreen@gmail.com>
2023-07-18 20:04:23 +0100

    Fixed some issues

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,20 +2,21 @@ const router = require('express').Router();
 const { Post, Comments } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-
-router.post('/post', withAuth, async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
   const body = req.body;
 
   try {
-    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    const newPost = await Post.create({
+      ...body,
+      user_id: req.session.user_id,
+    });
     res.json(newPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
-router.put('/post/:id', withAuth, async (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
   try {
     const [affectedRows] = await Post.update(req.body, {
       where: {
@@ -33,31 +34,34 @@ router.put('/post/:id', withAuth, async (req, res) => {
   }
 });
 
-
-router.delete('/post/:id', withAuth, async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const [affectedRows] = Post.destroy({
+    const PostData = await Post.destroy({
       where: {
         id: req.params.id,
+        user_id: req.session.user_id,
       },
     });
 
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
+    if (!PostData) {
+      res.status(404).json({ message: 'No Post found with this id!' });
+      return;
     }
+
+    res.status(200).json(PostData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-
 router.post('/comment', withAuth, async (req, res) => {
   const body = req.body;
 
   try {
-    const newComment = await Comments.create({ ...body, userId: req.session.userId });
+    const newComment = await Comments.create({
+      ...body,
+      userId: req.session.userId,
+    });
     res.json(newComment);
   } catch (err) {
     res.status(500).json(err);
@@ -81,7 +85,6 @@ router.put('/comment/:id', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
-
 
 router.delete('/comment/:id', withAuth, async (req, res) => {
   try {
