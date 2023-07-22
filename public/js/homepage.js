d769cb848b39cce24577f921042448a073b21390
daleyjones <djones_15@hotmail.com>
2023-07-22 22:00:17 +0100

    added articles

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -119,6 +119,60 @@ function displayEmbeddedPlaylist(playlistId) {
   playlistContainer.appendChild(iframe);
 }
 
+// Function to display the music article
+// Function to display the music article
+function displayMusicArticle(article) {
+    const articleContainer = document.getElementById("article-container");
+    let articleHTML = `
+        <div class="article">
+            <h2 class="article-title">${article.title}</h2>
+            <p class="article-content">${article.content}</p>
+        </div>`;
+    articleContainer.innerHTML += articleHTML; // Use += to append the article content
+}
+
+// Music article data (You can replace this with your own articles from the provided one)
+const musicArticle = {
+    title: "The Rhythm Titans: Unveiling the 10 Best Rock Drummers of All Time!",
+    content: `Hey there, fellow music enthusiasts! If you're like me, a die-hard rock music lover, you'll understand just how vital the role of a drummer is in any rock band. The heartbeat of the band, the pulse of the music – the drummers elevate rock anthems to an entirely different level! ... [The rest of the article content] ... Let the beats keep rolling, and the rock keep thundering! Rock on! :the_horns:`
+};
+
+const musicArticle2 = {
+    title: "Unraveling K-Pop: Why It's Unpopular to Predict It as the Music of the 2020s",
+    content: `Greetings, fellow music enthusiasts! As a music critic, it is my duty to explore the ever-evolving world of music and identify trends that might shape the soundscape of the new decade. Among the many genres vying for dominance, one has caught my attention: K-Pop. While acknowledging the greatness of rock, metal, and grunge, I must admit that predicting K-Pop as the music of the 2020s is, undoubtedly, an unpopular opinion. Why, you ask? Well, let's delve into the vibrant world of K-Pop and examine the reasons behind this controversial claim. ... [The rest of the article content] ... Let the music play on!`
+};
+
+const musicArticle3 = {
+    title: "In Search of Classic Blues: Is Modern Music Living Up to Its Legendary Past?",
+    content: `As an ardent classic blues fan, I can't help but wonder if I am alone in my sentiment that modern music simply doesn't hold a candle to the legendary sounds of yesteryears. The soul-stirring rhythms, heartfelt lyrics, and raw emotion of classic blues have left an indelible mark on my musical soul. But are today's tunes living up to that timeless magic? Let's explore this intriguing question. ... [The rest of the article content] ... After all, music's magic knows no bounds, and we might just stumble upon a contemporary masterpiece that leaves us breathless and in awe, just like the classic blues legends of old.`
+};
+
+const hipHopArticles = [
+    {
+      title: "The Evolution of Hip-Hop: From the Streets to Mainstream Dominance",
+      content: "Hip-hop, once an underground movement born in the streets of the Bronx, has undergone a remarkable transformation over the decades...",
+    },
+    {
+      title: "Sampling: The Art of Repurposing Music in Hip-Hop",
+      content: "A hallmark of hip-hop's creative process is sampling – the art of taking snippets from existing songs and incorporating them into new beats...",
+    },
+    {
+      title: "Hip-Hop's Social Impact: From Activism to Cultural Diplomacy",
+      content: "Hip-hop has long been a platform for artists to express their social and political views, championing causes and addressing pressing issues in society...",
+    }
+];
+
+// Display the music articles and hip-hop articles when the DOM is loaded
+document.addEventListener('DOMContentLoaded', function () {
+    displayMusicArticle(musicArticle);
+    displayMusicArticle(musicArticle2);
+    displayMusicArticle(musicArticle3);
+
+    hipHopArticles.forEach((article) => {
+        displayMusicArticle(article);
+    });
+});
+
 // Fetch and display the top tracks and recommended tracks
 document.addEventListener("DOMContentLoaded", async function () {
   displayTopTracks();
